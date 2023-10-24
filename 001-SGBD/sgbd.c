e95fdb24b22b3134d5c28c439dbbd72d4ee6e294
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2023-10-24 19:05:54 +0200

    v1 Guardar datos en archivo

--- 001-SGBD/sgbd.c
+++ 001-SGBD/sgbd.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    FILE *archivo;                           // creamos un elemento de tipo file denominado archivo
+    archivo = fopen("basededatos.txt", "w"); // abrimos fichero con paramentros: nombre del fichero y modo
+    char *texto = "SGBD";
+    fputs(texto, archivo); // puts guarda el texto en el fichero
+    fclose(archivo);       // cerramos fichero
     printf("SGBD");
     return 0;
 }
