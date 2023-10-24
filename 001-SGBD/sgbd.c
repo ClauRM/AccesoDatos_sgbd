fd807294eff94b4d534c74988df892bbf0b53d69
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2023-10-24 19:40:50 +0200

    v2 Guardar datos recibidos por parámetro de entrada

--- 001-SGBD/sgbd.c
+++ 001-SGBD/sgbd.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <string.h> //librería para tratamiento de texto
 
-int main()
+int main(int argc, char *argv[]) // pasar datos por parametro
 {
     FILE *archivo;                           // creamos un elemento de tipo file denominado archivo
-    archivo = fopen("basededatos.txt", "w"); // abrimos fichero con paramentros: nombre del fichero y modo
-    char *texto = "SGBD";
-    fputs(texto, archivo); // puts guarda el texto en el fichero
-    fclose(archivo);       // cerramos fichero
-    printf("SGBD");
+    archivo = fopen("basededatos.txt", "a"); // abrimos fichero con paramentros: nombre del fichero y modo (si es "a" lo aniade)
+    char *texto = argv[1];                   // el texto se captura como primer argumento
+    fputs(strcat(texto, "\n"), archivo);     // puts guarda el texto en el fichero
+    fclose(archivo);                         // cerramos fichero
     return 0;
 }
