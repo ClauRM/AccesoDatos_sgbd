047db96e78d1b586b9b820a6e2423c9a316a5689
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2023-10-25 13:14:51 +0200

    v4 Guardar datos especificando la base de datos, nombre de la tabla y dato a almacenar

--- 001-SGBD/sgbd.c
+++ 001-SGBD/sgbd.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
 #include <string.h> //libreria para tratamiento de texto
 
-//PARAMETROS: NOMBRE DEL FICHERO Y DATO A ALMACENAR
+// ARGUMENTOS: NOMBRE DE LA BASE DE DATOS, NOMBRE DE LA TABLA Y DATO A ALMACENAR
 
 int main(int argc, char *argv[]) // pasar datos por parametro
 {
     FILE *archivo; // creamos un elemento de tipo file denominado archivo
 
-    char *nombre = argv[1];                       // nombre del fichero primer parametro
-    char *nombrearchivo = strcat(nombre, ".txt"); // concateno para crear el nombre del fichero con su extension
+    char *basededatos = argv[1];   // nombre de la base de datos
+    char *nombrearchivo = argv[2]; // nombre de la tabla
+    char *texto = argv[3];         // dato a almacenar en el fichero
 
-    char rutaarchivo[100];              // defino variable para almacenar la ruta completa
-    strcpy(rutaarchivo, "DB/");         // copio "DB/" en rutaarchivo
-    strcat(rutaarchivo, nombrearchivo); // concateno el nombre del archivo en rutaarchivo
+    char rutaarchivo[100];                      // defino variable para almacenar la ruta completa
+    strcpy(rutaarchivo, basededatos);           // copio el nombre de la base de datos en la rutaarchivo
+    strcat(rutaarchivo, "-");                   // aniado guion para separar base de datos y tabla
+    strcat(nombrearchivo, ".txt");              // concateo con extension .txt
+    //printf(strcat(rutaarchivo, nombrearchivo)); // concateno el nombre del archivo en rutaarchivo
 
-    archivo = fopen(rutaarchivo, "a");   // abro fichero con paramentros: nombre del fichero y modo (si es "a" lo aniade)
-
-    char *texto = argv[2];               // el texto a agregar al fichero se captura como segundo argumento
+    archivo = fopen(strcat(rutaarchivo, nombrearchivo), "a");   // abro fichero con paramentros: nombre del fichero y modo (si es "a" lo aniade)
     fputs(strcat(texto, "\n"), archivo); // puts guarda el texto en el fichero
-
-    fclose(archivo);                     // cerramos fichero
+    fclose(archivo);                     // cierro fichero
 
     return 0;
-
-    // min18 intentar guardarlo dentro de la carpeta DB
 }
