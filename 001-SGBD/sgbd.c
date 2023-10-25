f2e038a9f8d02d3a15c3d4ad9a50018778f8c6bb
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2023-10-25 12:18:43 +0200

    v3 Guardar datos en tablas recibidos como parámetros de entrada

--- 001-SGBD/sgbd.c
+++ 001-SGBD/sgbd.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
-#include <string.h> //librería para tratamiento de texto
+#include <string.h> //libreria para tratamiento de texto
+
+//PARAMETROS: NOMBRE DEL FICHERO Y DATO A ALMACENAR
 
 int main(int argc, char *argv[]) // pasar datos por parametro
 {
-    FILE *archivo;                           // creamos un elemento de tipo file denominado archivo
-    archivo = fopen("basededatos.txt", "a"); // abrimos fichero con paramentros: nombre del fichero y modo (si es "a" lo aniade)
-    char *texto = argv[1];                   // el texto se captura como primer argumento
-    fputs(strcat(texto, "\n"), archivo);     // puts guarda el texto en el fichero
-    fclose(archivo);                         // cerramos fichero
+    FILE *archivo; // creamos un elemento de tipo file denominado archivo
+
+    char *nombre = argv[1];                       // nombre del fichero primer parametro
+    char *nombrearchivo = strcat(nombre, ".txt"); // concateno para crear el nombre del fichero con su extension
+
+    char rutaarchivo[100];              // defino variable para almacenar la ruta completa
+    strcpy(rutaarchivo, "DB/");         // copio "DB/" en rutaarchivo
+    strcat(rutaarchivo, nombrearchivo); // concateno el nombre del archivo en rutaarchivo
+
+    archivo = fopen(rutaarchivo, "a");   // abro fichero con paramentros: nombre del fichero y modo (si es "a" lo aniade)
+
+    char *texto = argv[2];               // el texto a agregar al fichero se captura como segundo argumento
+    fputs(strcat(texto, "\n"), archivo); // puts guarda el texto en el fichero
+
+    fclose(archivo);                     // cerramos fichero
+
     return 0;
+
+    // min18 intentar guardarlo dentro de la carpeta DB
 }
