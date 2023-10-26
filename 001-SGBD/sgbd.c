2894c120dab8cfc5917b21b4db5805960feac4b0
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2023-10-26 19:19:54 +0200

    v5 Implementación de opción where

--- 001-SGBD/sgbd.c
+++ 001-SGBD/sgbd.c
@@ -11,7 +11,9 @@ int main(int argc, char *argv[]) // pasar datos por parametro
     char *operacion = argv[1];     // tipo de operacion CRUD
     char *basededatos = argv[2];   // nombre de la base de datos
     char *nombrearchivo = argv[3]; // nombre de la tabla
-    char *texto = argv[4];         // dato a almacenar en el fichero
+    char *texto = argv[4];         // dato a almacenar o buscar en el fichero
+
+    char registro[200]; // variable para almacenar la cadena que hay en cada linea del fichero
 
     char rutaarchivo[100];              // defino variable para almacenar la ruta completa
     strcpy(rutaarchivo, basededatos);   // copio el nombre de la base de datos en la rutaarchivo
@@ -22,7 +24,6 @@ int main(int argc, char *argv[]) // pasar datos por parametro
     if (strcmp(operacion, "select") == 0) // strcmp compara dos string, si el resultado es 0 son iguales
     {
         archivo = fopen(rutaarchivo, "r"); // abro fichero con paramentros: nombre del fichero y modo (si es "a" lo aniade)
-        char registro[200];                // variable para almacenar la cadena que hay en cada linea del fichero
         int numero = 1;                    // variable para indicar el numero de registros que hay
         printf("Datos del fichero %s: \n", rutaarchivo);
         while (fgets(registro, 200, archivo) != NULL) // mientras que haya un registro a leer en el fichero, toma la longitud de max caracteres y no sea null
@@ -34,9 +35,22 @@ int main(int argc, char *argv[]) // pasar datos por parametro
     }
     else if (strcmp(operacion, "insert") == 0)
     {
-        archivo = fopen(rutaarchivo, "a");   // abro fichero con paramentros: nombre del fichero y modo (si es "a" lo aniade)
+        archivo = fopen(rutaarchivo, "a");   // modo "a" aniade
         fputs(strcat(texto, "\n"), archivo); // puts guarda el texto en el fichero
-        fclose(archivo);                     // cierro fichero
+        fclose(archivo);
+    }
+
+    else if (strcmp(operacion, "where") == 0)
+    {
+        archivo = fopen(rutaarchivo, "r");
+        while (fgets(registro, 200, archivo) != NULL) // mientras que haya un registro a leer en el fichero, toma la longitud de max caracteres y no sea null
+        {
+            if (strstr(registro, texto) != NULL)
+            { // busca el texto en el registro actual leido. Si encuentra coincidencia devuelve distinto de NULL
+                printf("Localizado registro: %s \n", registro);
+            }
+        }
+        fclose(archivo);
     }
     else
     {
