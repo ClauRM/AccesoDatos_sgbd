854eb52b6a8647b977789465d6ce7090b070b19b
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2023-10-26 18:54:45 +0200

    v5 Insertar y mostrar datos de la tabla base de datos y tabla indicadas

--- 001-SGBD/sgbd.c
+++ 001-SGBD/sgbd.c
@@ -1,25 +1,47 @@
 #include <stdio.h>
 #include <string.h> //libreria para tratamiento de texto
 
-// ARGUMENTOS: NOMBRE DE LA BASE DE DATOS, NOMBRE DE LA TABLA Y DATO A ALMACENAR
+// ARGUMENTOS: TIPO DE OPERACION, NOMBRE DE LA BASE DE DATOS, NOMBRE DE LA TABLA Y DATO A ALMACENAR
+// LEER Y ALMACENAR
 
 int main(int argc, char *argv[]) // pasar datos por parametro
 {
     FILE *archivo; // creamos un elemento de tipo file denominado archivo
 
-    char *basededatos = argv[1];   // nombre de la base de datos
-    char *nombrearchivo = argv[2]; // nombre de la tabla
-    char *texto = argv[3];         // dato a almacenar en el fichero
+    char *operacion = argv[1];     // tipo de operacion CRUD
+    char *basededatos = argv[2];   // nombre de la base de datos
+    char *nombrearchivo = argv[3]; // nombre de la tabla
+    char *texto = argv[4];         // dato a almacenar en el fichero
 
-    char rutaarchivo[100];                      // defino variable para almacenar la ruta completa
-    strcpy(rutaarchivo, basededatos);           // copio el nombre de la base de datos en la rutaarchivo
-    strcat(rutaarchivo, "-");                   // aniado guion para separar base de datos y tabla
-    strcat(nombrearchivo, ".txt");              // concateo con extension .txt
-    //printf(strcat(rutaarchivo, nombrearchivo)); // concateno el nombre del archivo en rutaarchivo
+    char rutaarchivo[100];              // defino variable para almacenar la ruta completa
+    strcpy(rutaarchivo, basededatos);   // copio el nombre de la base de datos en la rutaarchivo
+    strcat(rutaarchivo, "-");           // aniado guion para separar base de datos y tabla
+    strcat(nombrearchivo, ".txt");      // concateo con extension .txt
+    strcat(rutaarchivo, nombrearchivo); // concateno ruta de archivo con nombre del archio
 
-    archivo = fopen(strcat(rutaarchivo, nombrearchivo), "a");   // abro fichero con paramentros: nombre del fichero y modo (si es "a" lo aniade)
-    fputs(strcat(texto, "\n"), archivo); // puts guarda el texto en el fichero
-    fclose(archivo);                     // cierro fichero
+    if (strcmp(operacion, "select") == 0) // strcmp compara dos string, si el resultado es 0 son iguales
+    {
+        archivo = fopen(rutaarchivo, "r"); // abro fichero con paramentros: nombre del fichero y modo (si es "a" lo aniade)
+        char registro[200];                // variable para almacenar la cadena que hay en cada linea del fichero
+        int numero = 1;                    // variable para indicar el numero de registros que hay
+        printf("Datos del fichero %s: \n", rutaarchivo);
+        while (fgets(registro, 200, archivo) != NULL) // mientras que haya un registro a leer en el fichero, toma la longitud de max caracteres y no sea null
+        {
+            printf("%i: %s", numero, registro); // muestra el contenido
+            numero++;
+        }
+        fclose(archivo); // cierro fichero
+    }
+    else if (strcmp(operacion, "insert") == 0)
+    {
+        archivo = fopen(rutaarchivo, "a");   // abro fichero con paramentros: nombre del fichero y modo (si es "a" lo aniade)
+        fputs(strcat(texto, "\n"), archivo); // puts guarda el texto en el fichero
+        fclose(archivo);                     // cierro fichero
+    }
+    else
+    {
+        printf("ERROR. Opcion marcada no valida.");
+    }
 
     return 0;
 }
